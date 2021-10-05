41b2c0ca00bf0f5ee98f444ad5e6501c7f9be16c
Eric de Lira Candido <ericdelira2001@gmail.com>
2021-10-04 23:31:54 -0300

    Representação da janela e viewport

---
+++ window/Window.java
@@ -0,0 +1,56 @@
+package window;
+
+import point.Point;
+
+public class Window {
+    private int xMin, xMax, yMin, yMax;
+
+    public Window(){
+        setXmin(0);
+        setXmax(0);
+        setYmin(0);
+        setYmax(0);
+    }
+
+    public Window(int xwMin, int xMax, int yMin, int yMax){
+        super();
+        setXmin(xwMin);
+        setXmax(xMax);
+        setYmin(yMin);
+        setYmax(yMax);
+    }
+
+    /*public Point mapToViewport(Window viewport, Point point){
+        Point viewportPoint = new Point();
+
+        viewportPoint.setX(viewport.getXmin());
+
+        return viewportPoint;
+    }*/
+
+    /* setters and getters */
+    public int getXmin() {
+        return xMin;
+    }
+    public void setXmin(int xMin) {
+        this.xMin = xMin;
+    }
+    public int getYmin() {
+        return yMin;
+    }
+    public void setYmin(int yMin) {
+        this.yMin = yMin;
+    }
+    public int getXmax() {
+        return xMax;
+    }
+    public void setXmax(int xMax) {
+        this.xMax = xMax;
+    }
+    public int getYmax() {
+        return yMax;
+    }
+    public void setYmax(int yMax) {
+        this.yMax = yMax;
+    }
+}
