969865b84877f6b3bd26e1024fb104537b13fe93
Eric de Lira Candido <ericdelira2001@gmail.com>
2021-10-06 09:48:20 -0300

    Desenhar pontos na window e viewport.

---
+++ point/PointDrawings.java
@@ -0,0 +1,35 @@
+package point;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import window.Window;
+
+public class PointDrawings {
+
+    public static void drawPointOnWindow(Graphics2D g, int x, int y, String name, int diameter, Color color){
+        GraphicPoint point = new GraphicPoint(x, y, color, name, diameter);
+        point.drawPoint(g);
+    }
+
+    public static void drawPointOnViewport(Graphics2D g, Window window, Window viewport, int x, int y, String name, int diameter, Color color){
+
+        Point auxPoint = window.mapToViewport(viewport, new Point(x, y));
+
+        GraphicPoint point = new GraphicPoint((int)auxPoint.getX(), (int)auxPoint.getY(), Color.BLUE, name, diameter);
+
+        point.drawPoint(g);
+    }
+
+    public static void drawMultiplePoints(Graphics2D g, int qty, int diameter){
+        for(int i=0; i < qty; i++){
+            int x = (int) (Math.random() * 800);
+            int y = (int) (Math.random() * 800);
+
+            Color color = new Color((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256));
+
+            GraphicPoint point = new GraphicPoint(x, y, color, diameter);
+
+            point.drawPoint(g);
+        }
+    }
+}
