980b62490a7f204c47ddf9362c4166d35c9069b1
Eric de Lira Candido <ericdelira2001@gmail.com>
2021-10-07 12:29:11 -0300

    Criação dos tipos e constantes.

---
+++ constants/Constants.java
@@ -0,0 +1,18 @@
+package constants;
+
+public class Constants {
+    // Window dimensions
+    public static final int XW_MIN = 0;
+    public static final int YW_MIN = 0;
+    public static final int XW_MAX = 680;
+    public static final int YW_MAX = 510;
+
+    // Viewport dimensions
+    public static final int XV_MIN = 450;
+    public static final int YV_MIN = 350;
+    public static final int XV_MAX = 680;
+    public static final int YV_MAX = 510;
+
+    public static final int WIDTH = 850;
+    public static final int HEIGHT = 730;
+}
---
+++ constants/PrimitiveTypes.java
@@ -0,0 +1,5 @@
+package constants;
+
+public enum PrimitiveTypes {
+    POINT, LINE, CIRCLE, NONE
+}
